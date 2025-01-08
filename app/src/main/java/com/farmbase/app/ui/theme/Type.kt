7e738a6e342ef37509efdddee7c040a10ac8e85d
kingahmedino <ahmed.mohamed2@sterling.ng>
2025-01-08 11:40:01 +0100

    feat: Fix up theming, coloring

--- app/src/main/java/com/farmbase/app/ui/theme/Type.kt
+++ app/src/main/java/com/farmbase/app/ui/theme/Type.kt
@@ -2,33 +2,34 @@ package com.farmbase.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.farmbase.app.R
 
-// Set of Material typography styles to start with
+val EpilogueFontFamily = FontFamily(
+    Font(R.font.epilogue_regular, FontWeight.Normal),
+    Font(R.font.epilogue_medium, FontWeight.Medium),
+    Font(R.font.epilogue_bold, FontWeight.Bold),
+)
+
+val baseline = Typography()
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = baseline.displayLarge.copy(fontFamily = EpilogueFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = EpilogueFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = EpilogueFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.SemiBold),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.SemiBold),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.SemiBold),
+    titleLarge = baseline.titleLarge.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.SemiBold),
+    titleMedium = baseline.titleMedium.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.SemiBold),
+    titleSmall = baseline.titleSmall.copy(fontFamily = EpilogueFontFamily, fontWeight = FontWeight.Medium),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = EpilogueFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = EpilogueFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = EpilogueFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = EpilogueFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = EpilogueFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = EpilogueFontFamily),
 )
