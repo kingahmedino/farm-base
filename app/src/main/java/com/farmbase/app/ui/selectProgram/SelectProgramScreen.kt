0eab91f6e734ec9c432e69554aae137a4881ebcc
Derin <derin.bankole@babbangona.com>
2025-04-01 10:42:59 +0100

    #ASMPP-4: added implementation to display users programs

--- app/src/main/java/com/farmbase/app/ui/selectProgram/SelectProgramScreen.kt
+++ app/src/main/java/com/farmbase/app/ui/selectProgram/SelectProgramScreen.kt
@@ -1,71 +1,34 @@
 package com.farmbase.app.ui.selectProgram
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.farmbase.app.R
-import com.farmbase.app.ui.widgets.activityCardSection
-import com.farmbase.app.ui.widgets.NextButton
-import com.farmbase.app.ui.widgets.TopBar
+import com.farmbase.app.ui.formBuilder.utils.Resource
+import com.farmbase.app.utils.ErrorScreen
+import com.farmbase.app.utils.LoadingScreen
 
 @Composable
 fun SelectProgramScreen(
     onNextButtonClicked: () -> Unit = {},
     viewModel:SelectProgramViewModel = hiltViewModel()) {
-    val programList by viewModel.programList.collectAsStateWithLifecycle()
-    val selectedActivityCard by viewModel.selectedActivityCard.collectAsStateWithLifecycle()
 
-    Scaffold(modifier = Modifier,
-        topBar = {
-            TopBar(modifier = Modifier.fillMaxWidth()){}
-        },
-        bottomBar = { NextButton(
-            onClick = {onNextButtonClicked()},
-            enabled = selectedActivityCard != null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)) }
-    ) {paddingValues ->
-        LazyColumn ( modifier = Modifier
-            .padding(paddingValues)
-            .fillMaxSize()
-            .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-
-            item{
-                Text(
-                    text = stringResource(R.string.select_program),
-                    style = MaterialTheme.typography.labelLarge
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                Text(
-                    text = stringResource(R.string.select_program_desc),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-
-            activityCardSection(
-                itemList = programList,
-                isItemSelected = { selectedActivityCard == it },
-                onItemSelected = viewModel::updateSelectedCard,
-                shouldDividerShow = false
-            )
+    val programConfig by viewModel.programData.collectAsStateWithLifecycle()
+    when (programConfig) {
+        is Resource.Loading -> {
+            LoadingScreen()
         }
 
+        is Resource.Success -> {
+          ProgramDetailScreen(onNextButtonClicked)
+        }
+
+        is Resource.Error -> {
+            ErrorScreen(
+                message = "Unable to fetch program information"
+            )
+        }
     }
+
 }
+
--- app/src/main/java/com/farmbase/app/ui/selectProgram/SelectProgramViewModel.kt
+++ app/src/main/java/com/farmbase/app/ui/selectProgram/SelectProgramViewModel.kt
@@ -1,30 +1,72 @@
 package com.farmbase.app.ui.selectProgram
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.farmbase.app.R
+import com.farmbase.app.models.ProgramData
+import com.farmbase.app.ui.formBuilder.utils.Resource
+import com.farmbase.app.useCase.GetProgramDataByRolesUseCase
 import com.farmbase.app.utils.ActivityCardItem
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SelectProgramViewModel @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val programDataUseCase: GetProgramDataByRolesUseCase,
 ):ViewModel() {
+    private val roles =  listOf("67e00ad59b2b98774577c62a", "67e038e0b4320cde3f1cc247" )
+    private val _programData = MutableStateFlow<Resource<List<ProgramData>>>(Resource.Loading())
+    val programData: StateFlow<Resource<List<ProgramData>>> = _programData.asStateFlow()
+
+    init {
+        fetchProgramDetails()
+    }
+
+    /** This function fetches the details of available programs
+     * and updates the program list state.
+     * */
+    private fun fetchProgramDetails() {
+        viewModelScope.launch {
+            // set the program data to a loading state while the data is being fetched
+            _programData.value = Resource.Loading()
+
+            // invoke the use case to fetch the program data based on the logged in users role
+            _programData.value = programDataUseCase.invoke(roles)
+
+            // if the fetched data is successful, proceed to process the program list
+            (_programData.value as? Resource.Success)?.data?.let { programList ->
+                // create a mutable list to accumulate all the program items
+                val updatedProgramList = mutableListOf<ActivityCardItem>()
+
+                // iterates through each program data item from the fetched list
+                programList.forEach { programData ->
+                    // add each program data to the updated list.
+                    updatedProgramList.add(
+                        ActivityCardItem(
+                            id = programData.programId,
+                            iconUrl = programData.icon.trim(),
+                            icon = R.drawable.ic_back,
+                            headerText = programData.programName
+                        )
+                    )
+                }
+
+                // update the LiveData with the complete list of programs.
+                _programList.value = updatedProgramList
+            }
+        }
+    }
+
+
     // update selected activity card
     private val _selectedActivityCard = MutableStateFlow<ActivityCardItem?>(null)
     val selectedActivityCard = _selectedActivityCard.asStateFlow()
 
-    private val _programList = MutableStateFlow(
-        listOf(
-            ActivityCardItem(id = "1", icon = R.drawable.ic_personal_info, headerText = "Poultry Mother Unit Program"),
-            ActivityCardItem(id = "2",icon = R.drawable.ic_personal_info, headerText = "Maize Program"),
-        )
-    )
+    private val _programList = MutableStateFlow<List<ActivityCardItem>>(emptyList())
     val programList: StateFlow<List<ActivityCardItem>> = _programList.asStateFlow()
 
     /** Updates the selected activity card when a card is clicked
