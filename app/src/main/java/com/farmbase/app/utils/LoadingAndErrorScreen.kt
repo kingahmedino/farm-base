6764603c9f0412e3920dbd42c24bc2f13fb5cd49
Derin <derin.bankole@babbangona.com>
2025-04-02 14:04:43 +0100

    #ASMPP-4: modified loading screen to add text

--- app/src/main/java/com/farmbase/app/utils/LoadingAndErrorScreen.kt
+++ app/src/main/java/com/farmbase/app/utils/LoadingAndErrorScreen.kt
@@ -1,26 +1,38 @@
 package com.farmbase.app.utils
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 /**
  * A composable function that displays a loading screen with a circular progress indicator.
  * It fills the entire screen and centers the indicator.
  */
 @Composable
-fun LoadingScreen() {
+fun LoadingScreen(text: String, modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator()
+        Column (
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = text, style = MaterialTheme.typography.bodyMedium)
+        }
     }
 }
 
