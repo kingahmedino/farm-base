85823590ec7cc795f1e18cd1ab6dba16ce8e54be
Derin <derin.bankole@babbangona.com>
2025-04-02 14:52:50 +0100

    #ASMPP-8: added use case to fetch all unscheduled activities

---
+++ app/src/main/java/com/farmbase/app/useCase/GetUnscheduledActivitiesUseCase.kt
@@ -0,0 +1,36 @@
+package com.farmbase.app.useCase
+
+import com.farmbase.app.R
+import com.farmbase.app.repositories.ActivityEntityRepository
+import com.farmbase.app.utils.ActivityCardItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapNotNull
+import javax.inject.Inject
+
+/**
+ * Use case to fetch roles from the repository, sort them by the number of child portfolios (in descending order),
+ * and map each role to an ActivityCardItem. The result is returned as a Flow.
+ */
+class GetUnscheduledActivitiesUseCase @Inject constructor(
+    private val activitiesRepository: ActivityEntityRepository
+) {
+    /**
+     * Executes the use case to fetch, sort, and map roles to ActivityCardItem.
+     * @return Flow of a list of ActivityCardItem sorted by the number of child portfolios.
+     */
+    fun execute(): Flow<List<ActivityCardItem>> {
+        // fetch roles from the repository as a flow and transform them
+        return activitiesRepository.getAllUnscheduledActivities()
+            .mapNotNull { roles ->
+                // Transform each RoleEntity to an ActivityCardItem
+                roles.map { role ->
+                    ActivityCardItem(
+                        id = role.activityId,
+                        icon = R.drawable.ic_my_schedule,
+                        headerText = role.name
+                    )
+                }
+            }
+    }
+}
+
